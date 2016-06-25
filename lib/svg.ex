2573020102a46aaf6422b2a7fbbbaacf19af5816
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-25 18:50:48 -0400

    Remove SVG struct to simplify SVG function behavior

--- lib/svg.ex
+++ lib/svg.ex
@@ -1,67 +1,42 @@
 defmodule SVG do
-  defstruct nodes: []
-
-  def to_string(%SVG{nodes: nds}) do
-    nds
+  def to_string(node_list) do
+    node_list
     |> Enum.map(&SVGNode.to_string/1)
     |> Enum.join("")
   end
 
   def rect(x, y, width, height) do
     attrs = [x: x, y: y, width: width, height: height]
-    %SVG{
-      nodes: [%SVGNode{name: "rect", self_closing: true, attrs: attrs}]
-    }
+    [%SVGNode{name: "rect", self_closing: true, attrs: attrs}]
   end
 
   def circle(cx, cy, r) do
     attrs = [cx: cx, cy: cy, r: r]
-    %SVG{
-      nodes: [%SVGNode{name: "circle", self_closing: true, attrs: attrs}]
-    }
+    [%SVGNode{name: "circle", self_closing: true, attrs: attrs}]
   end
 
   def path(str) do
-    %SVG{
-      nodes: [%SVGNode{name: "path", self_closing: true, attrs: [d: str]}]
-    }
+    [%SVGNode{name: "path", self_closing: true, attrs: [d: str]}]
   end
 
   def polyline(str) do
-    %SVG{
-      nodes: [%SVGNode{name: "polyline", self_closing: true, attrs: [points: str]}]
-    }
+    [%SVGNode{name: "polyline", self_closing: true, attrs: [points: str]}]
   end
 
-  def get_node_list(svg_or_node_list) do
-    case svg_or_node_list do
-      %SVG{nodes: node_list} ->
-        node_list
-      node_list ->
-        node_list
-    end
-  end
-
-  def group(svg_or_node_list) do
-    %SVG{
-      nodes: List.flatten([
-        %SVGNode{name: "g"},
-        get_node_list(svg_or_node_list),
-        %SVGNode{name: "g", closing: true}
-      ])
-    }
+  def group(node_list) do
+    List.flatten([
+      %SVGNode{name: "g"},
+      node_list,
+      %SVGNode{name: "g", closing: true}
+    ])
   end
 
   def svg_header(width \\ 101, height \\ 101) do
     attrs = [xmlns: "http://www.w3.org/2000/svg", width: width, height: height]
-    %SVG{
-      nodes: [%SVGNode{name: "svg", attrs: attrs}]
-    }
+    [%SVGNode{name: "svg", attrs: attrs}]
   end
 
   def svg_footer do
-    %SVG{
-      nodes: [%SVGNode{name: "svg", closing: true}]
-    }
+    [%SVGNode{name: "svg", closing: true}]
   end
 end
