cebc000dfdfd4b72a7ee93a4bc60a0f2b2543000
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-25 00:30:25 -0400

    Create GeoPatternSpec and begin filling it with context blocks

--- spec/geo_pattern_spec.exs
+++ spec/geo_pattern_spec.exs
@@ -1,5 +1,26 @@
-defmodule ExampleSpec do
+defmodule GeoPatternSpec do
   use ESpec
+  doctest GeoPattern
+
+  describe "generate/2" do
+    context "when an invalid option is given" do
+    end
+
+    context "when a base color is given" do
+    end
+
+    context "when a color is given" do
+    end
+
+    context "when multiple patterns are given" do
+    end
+
+    context "when a valid pattern is given" do
+    end
+
+    context "when invalid patterns are given" do
+    end
+  end
 
   before do
     answer = Enum.reduce((1..9), &(&2 + &1)) - 3
