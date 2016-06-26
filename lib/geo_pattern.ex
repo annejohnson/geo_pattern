7759f4222a53f2c3f4b4710536f4407dfeabdc49
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-26 14:51:36 -0400

    Make GeoPattern generate the squares pattern

--- lib/geo_pattern.ex
+++ lib/geo_pattern.ex
@@ -4,6 +4,7 @@ defmodule GeoPattern do
   def generate(input_string, options \\ []) do
     svg_nodes = SVG.svg_header ++
                 SVG.background("#336699") ++
+                GeoPattern.Patterns.Squares.generate(input_string, options) ++
                 SVG.svg_footer
     SVG.to_string(svg_nodes)
   end
