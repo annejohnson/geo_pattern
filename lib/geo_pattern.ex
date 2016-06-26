329599b86a3da8633f12264a9ad10509ef556097
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-26 13:15:15 -0400

    Initial SVG string main generator function

--- lib/geo_pattern.ex
+++ lib/geo_pattern.ex
@@ -1,2 +1,10 @@
 defmodule GeoPattern do
+  alias GeoPattern.SVG
+
+  def generate(input_string, options \\ []) do
+    svg_nodes = SVG.svg_header ++
+                SVG.background("#336699") ++
+                SVG.svg_footer
+    SVG.to_string(svg_nodes)
+  end
 end
