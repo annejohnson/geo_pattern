fd1b72d279c2e873410ede4907ba8c2f648ffe18
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-26 16:00:10 -0400

    Make square size dynamic based on input

--- lib/geo_pattern/patterns/squares.ex
+++ lib/geo_pattern/patterns/squares.ex
@@ -1,5 +1,5 @@
 defmodule GeoPattern.Patterns.Squares do
-  alias GeoPattern.{SVG, Color}
+  alias GeoPattern.{SVG, Color, Utils}
 
   @num_squares_in_row 6
 
@@ -7,27 +7,30 @@ defmodule GeoPattern.Patterns.Squares do
     0..(@num_squares_in_row - 1)
   end
 
-  def square_width(total_width) do
-    round(total_width / @num_squares_in_row)
+  def square_width(input_string) do
+    min_width = 10
+    max_width = 60
+    hex_val = Utils.hex_val(input_string, 0)
+    Utils.remap(hex_val, 0, 15, min_width, max_width)
   end
 
-  def generate(input, options \\ []) do
+  def generate(input_string, options \\ []) do
+    square_size = square_width(input_string)
+
     row_and_column_range
     |> Enum.map(fn(row_index) ->
-         generate_row(row_index, options)
+         generate_row(input_string, row_index, square_size, options)
        end)
     |> List.flatten
   end
 
-  def generate_row(row_index, options \\ []) do
-    square_width = square_width(Keyword.get(options, :width, 101))
-
+  def generate_row(input_string, row_index, square_size, options \\ []) do
     Enum.map(row_and_column_range, fn(column_index) ->
       SVG.rect(
-        column_index * square_width,
-        row_index * square_width,
-        square_width,
-        square_width,
+        column_index * square_size,
+        row_index * square_size,
+        square_size,
+        square_size,
         fill: Color.to_svg("#33ff99"),
         fill_opacity: 1.0,
         stroke: Color.to_svg("#778899"),
--- lib/geo_pattern/utils.ex
+++ lib/geo_pattern/utils.ex
@@ -1,4 +1,10 @@
 defmodule GeoPattern.Utils do
+  alias GeoPattern.Seed
+
+  def hex_val(input_string, index, len \\ 1) do
+    Seed.to_integer(input_string, index, len)
+  end
+
   def remap(value, value_min, value_max, new_min, new_max) do
     value_float = value / 1
     value_range = value_max - value_min
