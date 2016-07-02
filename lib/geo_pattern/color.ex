78a3673ee6b17bf70f1cb6c52c89fc9c8189751a
Anne Johnson <annekjohnson23@yahoo.com>
2016-07-02 16:04:10 -0400

    Simplify Color module, extract rgb code into RGB module

--- lib/geo_pattern/color.ex
+++ lib/geo_pattern/color.ex
@@ -1,46 +1,11 @@
 defmodule GeoPattern.Color do
   alias GeoPattern.Color.RGB
 
-  @hex_max 255
+  def from_html(hex_string), do: RGB.new(hex_string)
 
-  def from_html(hex_string) do
-    [red, green, blue] =
-      hex_string
-      |> remove_non_hex_chars
-      |> String.split("", trim: true)
-      |> chunk_to_red_green_blue_strings
-      |> Enum.map(&hex_string_to_float/1)
-
-    %RGB{red: red, green: green, blue: blue}
-  end
-
-  def to_svg(%RGB{red: red, green: green, blue: blue}) do
-    [r, g, b] = [red, green, blue]
-                |> Enum.map(fn(color_decimal) -> color_decimal * @hex_max end)
-                |> Enum.map(&round/1)
-    ~s/rgb(#{r}, #{g}, #{b})/
-  end
-  def to_svg(hex_string) do
-    hex_string |> from_html |> to_svg
-  end
-
-  def hex_string_to_float(hex_string) do
-    {int, _} = Integer.parse(hex_string, 16)
-    int / @hex_max
-  end
-
-  def chunk_to_red_green_blue_strings(hex_chars) when length(hex_chars) == 6 do
-    hex_chars
-    |> Enum.chunk(2)
-    |> Enum.map(&Enum.join/1)
-  end
-  def chunk_to_red_green_blue_strings(hex_chars) when length(hex_chars) == 3 do
-    hex_chars
-    |> Enum.map(fn(char) -> char <> char end)
-  end
-
-  def remove_non_hex_chars(hex_string) do
+  def to_svg(hex_string) when is_binary(hex_string) do
     hex_string
-    |> String.replace(~r/[[:^xdigit:]]/, "")
+    |> from_html
+    |> RGB.to_svg
   end
 end
--- lib/geo_pattern/color/rgb.ex
+++ lib/geo_pattern/color/rgb.ex
@@ -1,3 +1,36 @@
 defmodule GeoPattern.Color.RGB do
   defstruct [:red, :green, :blue]
+  @hex_max 255
+
+  def new("#" <> hex_string) do
+    [red, green, blue] =
+      hex_string
+      |> String.split("", trim: true)
+      |> chunk_to_red_green_blue_strings
+      |> Enum.map(&hex_string_to_float/1)
+
+    %GeoPattern.Color.RGB{red: red, green: green, blue: blue}
+  end
+
+  def to_svg(%GeoPattern.Color.RGB{red: red, green: green, blue: blue}) do
+    [r, g, b] = [red, green, blue]
+                |> Stream.map(&(&1 * @hex_max))
+                |> Enum.map(&round/1)
+    ~s/rgb(#{r}, #{g}, #{b})/
+  end
+
+  defp hex_string_to_float(hex_string) do
+    {int, _} = Integer.parse(hex_string, 16)
+    int / @hex_max
+  end
+
+  defp chunk_to_red_green_blue_strings(hex_chars) when length(hex_chars) == 6 do
+    hex_chars
+    |> Stream.chunk(2)
+    |> Stream.map(&Enum.join/1)
+  end
+  defp chunk_to_red_green_blue_strings(hex_chars) when length(hex_chars) == 3 do
+    hex_chars
+    |> Stream.map(&(&1 <> &1))
+  end
 end
