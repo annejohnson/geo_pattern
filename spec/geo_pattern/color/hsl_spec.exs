76d0c2fa346a61584dbf87ccc068763d922fd789
Anne Johnson <annekjohnson23@yahoo.com>
2016-07-02 18:10:52 -0400

    Expand HSL test coverage

--- spec/geo_pattern/color/hsl_spec.exs
+++ spec/geo_pattern/color/hsl_spec.exs
@@ -2,23 +2,53 @@ defmodule GeoPattern.Color.HSLSpec do
   use ESpec
   doctest GeoPattern.Color.HSL
 
-  let :delta, do: 0.0001
+  let :delta, do: 0.5
 
   describe "new/1" do
     let :color_data do
       [
+        # blue
+        %{
+          hex_string: "#adc7ff",
+          hue: 221.0,
+          saturation: 100.0,
+          lightness: 84.0
+        },
+        # green
         %{
           hex_string: "#115500",
           hue: 108.0,
           saturation: 100.0,
-          lightness: 16.666667
+          lightness: 16.7
         },
+        # yellow
+        %{
+          hex_string: "#e3e345",
+          hue: 60.0,
+          saturation: 74.0,
+          lightness: 58.0
+        },
+        # red orange
+        %{
+          hex_string: "#8d5c44",
+          hue: 20.0,
+          saturation: 35.0,
+          lightness: 41.0
+        },
+        # red violet
         %{
           hex_string: "#b8194e",
           hue: 340.0,
-          saturation: 76.076555,
-          lightness: 40.9803921
-        }
+          saturation: 76.1,
+          lightness: 41.0
+        },
+        # gray
+        %{
+          hex_string: "#444",
+          hue: 0.0,
+          saturation: 0.0,
+          lightness: 27.0
+        },
       ]
     end
 
