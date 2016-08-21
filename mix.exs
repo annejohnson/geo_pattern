d4a5ba0ef160df4034eaf5ef6f1cc7344c7830ad
Anne Johnson <annekjohnson23@yahoo.com>
2016-08-21 19:56:25 -0400

    Update mix.exs in prep for publishing to Hex

--- mix.exs
+++ mix.exs
@@ -8,27 +8,27 @@ defmodule GeoPattern.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
-     deps: deps]
+     description: description,
+     deps: deps,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    "An Elixir library for generating beautiful SVG patterns from strings."
+  end
+
   defp deps do
     [{:espec, "~> 0.8.21", only: :test},
      {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [licenses: ["MIT"],
+     maintainers: ["annecodes@gmail.com"],
+     links: %{"GitHub" => "https://github.com/annejohnson/geo_pattern"}]
   end
 end
